11ac83f823d5299be36021cd54f1604ba0b8b9ed
redtide <redtid3@gmail.com>
2020-04-21 15:09:35 +0200

    Updated setup.sh (WIP)

--- docs/setup.sh
+++ docs/setup.sh
@@ -1,30 +1,50 @@
 #!/usr/bin/env bash
 set -e
-cd $PWD
+cd ${PWD}
 
-if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
-	echo "Setup and run Jekyll"
+usage()
+{
 	echo ""
 	echo "Usage: ${0} [option]"
+	echo "Setup and run Jekyll"
 	echo ""
 	echo "Options are not mandatory, only one at a time."
-	echo "-a, --assets      Build minimized css style and js script from sources."
-	echo "-i, --install     Install Bundler and node modules using Yarn."
-	echo "-d, --doxygen     Build Doxygen documentation"
+	echo "-a, --assets    Build minimized css style and js script from sources."
+	echo "-i, --install   Install Bundler and node modules using Yarn."
+	echo "-d, --doxygen   Build Doxygen documentation"
 	echo ""
-	exit 0
-fi
-if [ ! -d "node_modules" ] || [ "$1" == "-i" ] || [ "$1" == "--install" ]; then
+}
+assets_build()
+{
+	mkdir -p assets/js
+	yarn dist
+}
+install()
+{
 	gem update
 	gem install bundler
 	bundle install
 	yarn --no-bin-links
+}
+# FIXME: Process commands first, then handle directories absence
+if [ "${1}" == "--help" ] || [ "${1}" == "-h" ]; then
+	usage
+	exit 0
 fi
-if [ ! -f "assets/css/style.min.css" ] || [ "$1" == "-a" ] || [ "$1" == "--assets" ]; then
-	mkdir -p assets/js
-	yarn dist
+if [ ! -d "node_modules" ] || [ "${1}" == "-i" ] || [ "${1}" == "--install" ]; then
+	install
+	shift
 fi
-if [ ! -d "api" ] || [ "$1" == "-d" ] || [ "$1" == "--doxygen" ]; then
+if [ ! -f "assets/css/style.min.css" ] || [ "${1}" == "-a" ] || [ "${1}" == "--assets" ]; then
+	assets_build
+	shift
+fi
+if [ ! -d "api" ] || [ "${1}" == "-d" ] || [ "${1}" == "--doxygen" ]; then
 	python3 scripts/doxygen/doxy2json.py
+	shift
 fi
-bundle exec jekyll serve --watch --host=0.0.0.0
+if [ "${1}" == "-b" ] || [ "${1}" == "--baseurl" ]; then
+	shift
+	baseurl="--baseurl ${1}"
+fi
+bundle exec jekyll serve --watch --host=0.0.0.0 ${baseurl}
